d4e0fb7ac066e1ec28a97b42d0bae76c42c3eb02
Operador <jcasal@ubuntu.(none)>
2011-07-05 00:54:41 +0200

    primera solucion

---
+++ jacarui/primefactors/src/PrimeFactors.groovy
@@ -0,0 +1,29 @@
+import java.util.Iterator;
+
+import javax.swing.JComponent.IntVector;
+
+class PrimeFactors {
+
+	int[] generate(int number) {
+		def primeFactors = []
+		def multiples = 2..number
+		def iterator = multiples.iterator()
+		def multiple = iterator.next()
+		
+		while (number > 1) {			
+			if (isPrimeFactor(number, multiple)) {
+				primeFactors << multiple
+				number = number.intdiv(multiple)
+			} else {
+				multiple = iterator.next()
+			}
+		}
+					
+		primeFactors
+	}
+	
+	private isPrimeFactor(number, factor) {
+		number % factor == 0
+	}
+
+}
