c3986334a7c1a6b3771e533dbe508129effb4ba0
lantoli <leo.antoli@gmail.com>
2011-07-04 14:36:02 +0200

    some performance tips

--- lantoli/lib/primefactors.rb
+++ lantoli/lib/primefactors.rb
@@ -2,16 +2,24 @@ class PrimeFactors
 
   def self.generate number
       factors = []
-      limit = Math.sqrt number   # performance tip
-      (2..limit).each do |n|
-        while (number % n == 0)
-          number /= n
-          factors << n
+      limit = Math.sqrt(number).to_i   # performance tip: go up to square root
+      candidate = 2
+      while candidate <= limit
+        if candidate.factor_of? number
+          factors << candidate
+          number /= candidate
+        else
+          candidate = (candidate==2) ? 3 : candidate + 2 # performance tip: only odd candidates
         end
       end
-      factors << number unless number < limit
+      factors << number unless number <= limit # performance tip
       factors
   end
 
 end
 
+class Fixnum
+  def factor_of? number
+    number % self == 0
+  end
+end
