56b70ec48db88029e3e5b22f2234eacf959043c8
lantoli <leo.antoli@gmail.com>
2011-07-04 13:34:22 +0200

    go up to square root

--- lantoli/lib/primefactors.rb
+++ lantoli/lib/primefactors.rb
@@ -2,13 +2,14 @@ class PrimeFactors
 
   def self.generate number
       factors = []
-      partial = number
-      (2..number).each do |n|
-        while (partial % n == 0)
-          partial /= n
+      limit = Math.sqrt number   # performance tip
+      (2..limit).each do |n|
+        while (number % n == 0)
+          number /= n
           factors << n
         end
       end
+      factors << number unless number < limit
       factors
   end
 
