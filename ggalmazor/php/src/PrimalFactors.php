e36246ea5127ecf71e2ebc3564437198888ed9ee
Guillermo <guille@nianoniano.com>
2011-07-05 09:41:36 +0200

    Refactorizado y optimizada la obtención del siguiente cociente

--- ggalmazor/php/src/PrimalFactors.php
+++ ggalmazor/php/src/PrimalFactors.php
@@ -3,12 +3,18 @@
 class PrimalFactors {
 
   public static function of($number) {
-    $factors = array(); $quotient = 2;
-    while ($number > 1 && $quotient++) {
+    $factors = array();
+    $quotient = 2;
+    while ($number > 1) {
       while ($number % $quotient == 0)
         $factors[] = $quotient && $number /= $quotient;
+      $quotient = self::getNextQuotient($quotient, $number);
     }
     return $factors;
+  }
+  
+  private static function getNextQuotient($q, $n) {
+    return $q > sqrt($n) ? $n : $q + 1;
   }
 
 }
