5a0992896476d0ac4d14953a50556e6e3f92a3b5
lantoli <leo.antoli@gmail.com>
2011-07-05 09:07:02 +0200

    integer square root

--- lantoli/lib/primefactors.rb
+++ lantoli/lib/primefactors.rb
@@ -2,13 +2,13 @@ class PrimeFactors
 
   def self.generate number
       factors = []
-      limit = Math.sqrt(number).to_i   # performance tip: go up to square root
+      limit = number.sqrt  # performance tip: go up to square root
       candidate = 2
       while candidate <= limit && number > 1
         if candidate.factor_of? number
           factors << candidate
           number /= candidate
-          limit = Math.sqrt(number).to_i   # performance tip: go up to square root
+          limit = number.sqrt   # performance tip: go up to square root
         else
           candidate = (candidate==2) ? 3 : candidate + 2 # performance tip: only odd candidates
         end
@@ -22,5 +22,9 @@ end
 class Integer
   def factor_of? number
     number % self == 0
+  end
+ 
+  def sqrt
+    Math.sqrt(self).to_i
   end
 end
