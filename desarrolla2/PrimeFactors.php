b35b60abb6843d10e6aa3466bfb299612b68fcb6
Daniel González <daniel.gonzalez@radmas.com>
2011-07-11 09:00:58 +0200

    translate kata to spanish

---
+++ desarrolla2/PrimeFactors.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PrimeFactors
+ *
+ * @author dgonzalez
+ */
+class PrimeFactors {
+    //put your code here
+}
---
+++ desarrolla2/test/PrimeFactorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+require_once "../PrimeFactor.php";
+
+/**
+ * Description of PrimeFactorTest
+ *
+ * @author dgonzalez
+ */
+
+class PrimeFactorTest extends PHPUnit_framework_TestCase {
+    //put your code here
+}
