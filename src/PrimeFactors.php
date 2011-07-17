405b43b34f8b356f21c7406cda52f4fdee1f2823
Miguel San Román <endymiion@Hyperion.(none)>
2011-07-18 00:34:55 +0200

    Versión refactorizada de la kata del mes de Julio de 12meses12katas

---
+++ src/PrimeFactors.php
@@ -0,0 +1,32 @@
+<?php
+
+class PrimeFactors {
+
+    public static function generate($integer)
+    {
+        $result = array();
+        if ($integer > 1) {
+	    $noFactorFound = true;
+            for ($factor = 2; $factor <= $integer && $noFactorFound; ++$factor){
+                if(PrimeFactors::isFactor($integer, $factor)){
+                    $result = array_merge(array($factor), PrimeFactors::generate($integer/$factor));
+                    $noFactorFound = false;
+                }
+            }
+        }
+        return $result;
+    }
+
+    private function isFactor($integer, $factor)
+    {
+        return PrimeFactors::isDivisible($integer,$factor);
+    }
+
+    private function isDivisible($integer, $factor)
+    {
+        return $integer % $factor == 0;
+    }
+
+}
+
+?>
