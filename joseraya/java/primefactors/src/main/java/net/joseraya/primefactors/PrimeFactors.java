0d4b6e3d2854618f36c04490086c3d6eca8ee948
Jose Raya <mail@joseraya.net>
2011-07-20 14:21:36 +0200

    Implemented basic decomposition

--- joseraya/java/primefactors/src/main/java/net/joseraya/primefactors/PrimeFactors.java
+++ joseraya/java/primefactors/src/main/java/net/joseraya/primefactors/PrimeFactors.java
@@ -1,15 +1,23 @@
 package net.joseraya.primefactors;
 
-import java.util.LinkedList;
 import java.util.List;
+
+import org.fest.util.Collections;
 
 public class PrimeFactors {
 	public static List<Integer> generate(Integer source) {
 		if (source<2) {
 			throw new IllegalArgumentException("The number " + source + "cannot be decomposed in primes");
 		}
-		List<Integer> result = new LinkedList<Integer>();
-		result.add(source);
+		List<Integer> result = Collections.list();
+		if (source%2==0) {
+			result.add(2);
+			source = source/2;
+		}
+		if (source%3==0) {
+			result.add(3);
+			source = source/3;
+		}
 		return result;
 	}
 }
--- joseraya/java/primefactors/src/test/java/net/joseraya/primefactors/PrimeFactorsTest.java
+++ joseraya/java/primefactors/src/test/java/net/joseraya/primefactors/PrimeFactorsTest.java
@@ -2,6 +2,7 @@ package net.joseraya.primefactors;
 
 import java.util.List;
 
+import org.fest.util.Collections;
 import org.junit.Test;
 import static net.joseraya.primefactors.PrimeFactors.*;
 import static org.junit.Assert.*;
@@ -21,9 +22,16 @@ public class PrimeFactorsTest {
 	@Test
 	public void a_prime_should_return_itself() {
 		Integer number = 2;
+		List<Integer> expected = Collections.list(2);
 		result = generate(number);
-		assertEquals("A prime results in a single result list", 1, result.size());
-		assertEquals("A prime contains itself", number, result.get(0));
+		assertEquals(expected, result);
 	}
 
+	@Test
+	public void a_decomposable_number_should_be_decomposed() {
+		Integer number = 6;
+		List<Integer> expected = Collections.list(2,3);
+		result = generate(number);
+		assertEquals(expected, result);
+	}
 }
