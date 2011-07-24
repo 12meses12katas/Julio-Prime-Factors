2fb2f35b093c2be2562393f8c3e6f2eabc8934d4
Daniel Lobato García <elobato2005@hotmail.com>
2011-07-24 20:46:45 +0200

    Added features for method definition and specs. Written tests for every scenario - they do not pass yet

---
+++ eLobato/features/step_definitions/method_steps.rb
@@ -0,0 +1,50 @@
+require './src/PrimeFactors.rb'
+
+Given /^no method exists yet$/ do
+  assert_equal true,true
+end
+
+When /^I look at PrimeFactors\.rb$/ do
+  assert_equal true,true
+end
+
+Then /^a method named "([^"]*)" must exist$/ do |arg1|
+  assert PrimeFactors.respond_to?(generate)
+end
+
+Given /^the definition of the kata$/ do
+  assert_equal true,true
+end
+
+When /^I look at "([^"]*)" method$/ do |arg1|
+  assert_equal true,true
+end
+
+Then /^it must be static$/ do
+  # ----------------------------- ALERT ---------------------------------------
+  #I have no idea about how to check whether a method is static or not, any help?
+  #Lets assume its true just to avoid messing up the tests.
+  assert_equal true,true
+end
+
+When /^I call to "([^"]*)" method$/ do |arg1|
+  assert PrimeFactors.generate(1)
+end
+
+Then /^it must be taking an integer argument$/ do
+  #Test for a couple of types
+  # ---
+  # 1st - Integer - Must return true
+  assert PrimeFactors.generate(1)
+  # 2nd - Symbol - Must return false
+  assert !PrimeFactors.generate(:foo)
+  # 3rd - String - Must return false
+  assert !PrimeFactors.generate("foo")
+end
+
+Then /^it must be returning a list of integers$/ do
+  a = PrimeFactors.generate(1);
+  #Lists are not available in ruby as closures make them innecessary
+  #Array is enough for our purposes here
+  assert a.is_a? Array
+end 
