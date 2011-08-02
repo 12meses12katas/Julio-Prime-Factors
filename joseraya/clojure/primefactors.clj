3fbb836b61d4ffbd066adea8c5937f9f61be66e2
Jose Raya <mail@joseraya.net>
2011-08-02 23:33:21 +0200

    added 2,2 test case

--- joseraya/clojure/primefactors.clj
+++ joseraya/clojure/primefactors.clj
@@ -2,9 +2,17 @@
    (:use clojure.test))
    
 (defn generate [x] 
-  [x]
+	(if (= 2 x) [2]
+  		(if (zero? (rem x 2)) (conj (generate (/ x 2)) 2))
+  	)
 )
+
 (deftest test-two-returns-two
   (is (= [2]  (generate 2))))
+
+(deftest test-four-returns-two-twice
+  (is (= [2,2]  (generate 4))))
   
+(deftest test-more-than-one-factor
+  (is (= [2,3]  (generate 6))))
 (run-tests)
